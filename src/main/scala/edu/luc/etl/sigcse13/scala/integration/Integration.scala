e6218bc3758d440d0356ebeab2eccadba6ff2f5d
George K. Thiruvathukal <gkt@cs.luc.edu>
2013-03-05 20:19:22 -0600

    integration working the way it should

--- src/main/scala/edu/luc/etl/sigcse13/scala/integration/Integration.scala
+++ src/main/scala/edu/luc/etl/sigcse13/scala/integration/Integration.scala
@@ -8,7 +8,7 @@ object Integration {
   // begin-integrate
   def integrate(a: Double, b: Double, g: Int, f: Fx): Double = {
     val interval = (b - a) / g
-    val fxValues = (1 until g) map { n => f(a + n * interval) }
+    val fxValues = (1 until g).view map { n => f(a + n * interval) }
     (b - a) / g * (f(a) / 2 + f(b) / 2 + fxValues.sum)
   }
   // end-integrate
@@ -16,7 +16,7 @@ object Integration {
   // begin-integrateParallel
   def integrateParallel(a: Double, b: Double, g: Int, f: Fx): Double = {
     val interval = (b - a) / g
-	val fxValues = (1 until g).par.map { n => f(a + n * interval) }
+	val fxValues = (1 until g par).view map { n => f(a + n * interval) }
    	(b - a) / g * (f(a) / 2 + f(b) / 2 + fxValues.sum)
   }
   // end-integrateParallel
--- src/main/scala/edu/luc/etl/sigcse13/scala/integration/Main.scala
+++ src/main/scala/edu/luc/etl/sigcse13/scala/integration/Main.scala
@@ -22,7 +22,23 @@ object Main extends {
     }
   }
 
+  def timeThis[A](s : String)(block : => A) : A = {
+  	val time0 = System.currentTimeMillis
+    val b = block
+  	val time1 = System.currentTimeMillis - time0
+  	println("Timing " + s + " = " + time1)
+  	b
+  }
+
   // begin-timedRun
+<<<<<<< local
+  def timedRunSquare(rectangles : Int, n : Int) {
+  	// run it once to see whether we get any artifacts
+
+  	timeThis("sequential integrate()") {
+       val area = integrate(0, 10, rectangles, sqr)
+       println("Area = " + area)
+=======
   def timedRunSquare(rectangles : Int, n : Int, how: String,
       integrationStrategy: (Double, Double, Int, Fx) => Double) {
     val area = integrationStrategy(0, 10, rectangles, sqr)
@@ -30,10 +46,32 @@ object Main extends {
     val time0 = System.currentTimeMillis
     (1 to n) foreach { _ =>
     	val area = integrationStrategy(0, 10, rectangles, sqr)
+>>>>>>> other
     }
+<<<<<<< local
+
+  	timeThis("sequential integrate() " + n + " times") {
+       (1 to n) foreach { _ =>
+          val area = integrate(0, 10, rectangles, sqr)
+       }
+    }
+
+    timeThis("parallel integrate()") {
+       val area = integrateParallel(0, 10, rectangles, sqr)
+       println("Area = " + area)
+    }
+
+    timeThis("parallel integrate() " + n + " times") {
+       (1 to n) foreach { _ =>
+          val area = integrateParallel(0, 10, rectangles, sqr)
+       }
+    }
+
+=======
     val time1 = System.currentTimeMillis - time0
     println("total time = " + time1)
     println("average time = " + (time1.toFloat / n).toInt)
+>>>>>>> other
   }
   // end-timedRun
 }
