40018724c3428032593a998957219cd943eb2b93
George K. Thiruvathukal <gkt@cs.luc.edu>
2013-03-07 11:52:14 -0600

    added one more integrate function to show the effect of combining sequential and parallel

--- src/main/scala/edu/luc/etl/sigcse13/scala/integration/Integration.scala
+++ src/main/scala/edu/luc/etl/sigcse13/scala/integration/Integration.scala
@@ -21,5 +21,15 @@ object Integration {
   }
   // end-integrateParallel
 
+ // begin-integrateParallelGranular
+  def integrateParallelGranular(a: Double, b: Double, g: Int, grainSize : Int, f: Fx): Double = {
+    require { g % grainSize == 0 } // can relax this later
+    val interval = (b - a) / grainSize
+    val workers = g / grainSize
+    val fullIntegration = (0 until workers).par.map { n => integrate(a + n * interval, a + (n + 1) * interval, grainSize, f) }
+    fullIntegration.sum
+  }
+  // end-integrateParallelGranular
+
 }
 // end-object-Integration
--- src/main/scala/edu/luc/etl/sigcse13/scala/integration/Main.scala
+++ src/main/scala/edu/luc/etl/sigcse13/scala/integration/Main.scala
@@ -7,19 +7,26 @@ object Main extends {
 
   def main(args: Array[String]) {
     try {
-      require { 2 == args.length }
+      require { 2 <= args.length }
       val rectangles = math.max(args(0).toInt, 1000)
       val n  = math.max(args(1).toInt, 1)
-      timedRun(rectangles, 1, "priming the pump by running sequentially once", integrate)
+      val grainSize = if (args.length == 3) math.min(args(2).toInt, rectangles) else rectangles
+
       timedRun(rectangles, n, "sequentially", integrate)
       timedRun(rectangles, n, "in parallel", integrateParallel)
+
+      def bindNumWorkersToIntegrateGranular(a : Double, b : Double, g : Int, f : Fx) : Double = {
+        integrateParallelGranular(a, b, g, grainSize, f)
+      }
+      timedRun(rectangles, n, "in parallel with " + grainSize + " rectangles per serial worker", bindNumWorkersToIntegrateGranular)
+
     } catch {
       case _: NumberFormatException => usage()
       case _: IllegalArgumentException => usage()
     }
   }
 
-  def usage() { Console.err.println("usage: rectangles (>= 1000) numberOfRuns (>= 1)") }
+  def usage() { Console.err.println("usage: rectangles (>= 1000) numberOfRuns (>= 1) [ grainSize (rectangles % grainSize == 0) ]") }
 
   def timeThis[A](s : String)(block : => A) : A = {
   	val time0 = System.currentTimeMillis
@@ -34,7 +41,7 @@ object Main extends {
       integrationStrategy: (Double, Double, Int, Fx) => Double) {
     timeThis(how) {
        print("Computing area " + how + "; now timing " + n + " iterations")
-       val area: Double = (1 to 10).map { _ => integrationStrategy(0, 10, rectangles, sqr) }.head
+       val area: Double = (1 to n).map { _ => integrationStrategy(0, 10, rectangles, sqr) }.head
        println("; area = " + area)
     }
   }
