3e9536da64fbbef8ebc4da0b8a0eb9686ab6cdf8
George K. Thiruvathukal <gkt@cs.luc.edu>
2013-03-08 12:24:48 -0700

    some reformatting for the notes

--- src/main/scala/edu/luc/etl/sigcse13/scala/integration/Integration.scala
+++ src/main/scala/edu/luc/etl/sigcse13/scala/integration/Integration.scala
@@ -3,7 +3,9 @@ package edu.luc.etl.sigcse13.scala.integration
 // begin-object-Integration
 object Integration {
 
+  // begin-Fx
   type Fx = Double => Double
+  // end-Fx
 
   // begin-integrate
   def integrateSequential(a: Double, b: Double, rectangles: Int, f: Fx): Double = {
@@ -16,12 +18,12 @@ object Integration {
   // begin-integrateParallel
   def integrateParallel(a: Double, b: Double, rectangles: Int, f: Fx): Double = {
     val interval = (b - a) / rectangles
-	val fxValues = (1 until rectangles).par.view map { n => f(a + n * interval) }
-   	(b - a) / rectangles * (f(a) / 2 + f(b) / 2 + fxValues.sum)
+    val fxValues = (1 until rectangles).par.view map { n => f(a + n * interval) }
+    (b - a) / rectangles * (f(a) / 2 + f(b) / 2 + fxValues.sum)
   }
   // end-integrateParallel
 
- // begin-integrateParallelGranular
+  // begin-integrateParallelGranular
   def integrateParallelGranular(grainSize: Int)(a: Double, b: Double, rectangles: Int, f: Fx): Double = {
     require { rectangles % grainSize == 0 } // can relax this later
     val workers = rectangles / grainSize
--- src/main/scala/edu/luc/etl/sigcse13/scala/integration/Main.scala
+++ src/main/scala/edu/luc/etl/sigcse13/scala/integration/Main.scala
@@ -9,7 +9,7 @@ object Main extends {
     try {
       require { 2 <= args.length }
       val rectangles = math.max(args(0).toInt, 1000)
-      val n  = math.max(args(1).toInt, 1)
+      val n = math.max(args(1).toInt, 1)
       val grainSize = if (args.length == 3) math.min(args(2).toInt, rectangles) else rectangles
 
       timedRun(rectangles, n, "sequentially", integrateSequential)
@@ -28,21 +28,21 @@ object Main extends {
       "numberOfRuns (>= 1) [ grainSize (rectangles % grainSize == 0) ]")
   }
 
-  def timeThis[A](s : String)(block : => A) : A = {
-  	val time0 = System.currentTimeMillis
+  def timeThis[A](s: String)(block: => A): A = {
+    val time0 = System.currentTimeMillis
     val b = block
-  	val time1 = System.currentTimeMillis - time0
-  	println("Timing " + s + " = " + time1)
-  	b
+    val time1 = System.currentTimeMillis - time0
+    println("Timing " + s + " = " + time1)
+    b
   }
 
   // begin-timedRun
-  def timedRun(rectangles : Int, n : Int, how: String,
-      integrationStrategy: (Double, Double, Int, Fx) => Double) {
+  def timedRun(rectangles: Int, n: Int, how: String,
+    integrationStrategy: (Double, Double, Int, Fx) => Double) {
     timeThis(how) {
-       print("Computing area " + how + "; now timing " + n + " iterations")
-       val area: Double = (1 to n).map { _ => integrationStrategy(0, 10, rectangles, sqr) }.head
-       println("; area = " + area)
+      print("Computing area " + how + "; now timing " + n + " iterations")
+      val area: Double = (1 to n).map { _ => integrationStrategy(0, 10, rectangles, sqr) }.head
+      println("; area = " + area)
     }
   }
   // end-timedRun
